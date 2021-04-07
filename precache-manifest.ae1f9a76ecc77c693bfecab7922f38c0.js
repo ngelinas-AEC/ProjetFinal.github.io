621c136430d4c5188dfdd7cafab74c032caf1532
ngelinas-AEC <57510354+ngelinas-AEC@users.noreply.github.com>
2021-04-06 21:36:03 -0400

    Add files via upload
    commit

---
+++ precache-manifest.ae1f9a76ecc77c693bfecab7922f38c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f825b91235f56a34bd076a01fdcd50ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67bf52ac4c03674e708c",
+    "url": "./static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "4131f2382b3f6f332c02",
+    "url": "./static/css/main.38b94a96.chunk.css"
+  },
+  {
+    "revision": "67bf52ac4c03674e708c",
+    "url": "./static/js/2.bb96787a.chunk.js"
+  },
+  {
+    "revision": "33680cec0d310757f5a44620ee836cb0",
+    "url": "./static/js/2.bb96787a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4131f2382b3f6f332c02",
+    "url": "./static/js/main.38a8a46e.chunk.js"
+  },
+  {
+    "revision": "89145541f65879a00cdb",
+    "url": "./static/js/runtime-main.bdc10778.js"
+  },
+  {
+    "revision": "2041535998fd3b7119929e871f181949",
+    "url": "./static/media/screen1_img1.20415359.png"
+  },
+  {
+    "revision": "d9bbe29db842d3cbd237b7f489819452",
+    "url": "./static/media/screen1_img6.d9bbe29d.png"
+  },
+  {
+    "revision": "832341170441dd1f90c5018be227ba72",
+    "url": "./static/media/screen1_img7.83234117.png"
+  },
+  {
+    "revision": "6b7d7b3b1cb4cd2a9bd94cd72bc15ee7",
+    "url": "./static/media/screen1_img8.6b7d7b3b.png"
+  },
+  {
+    "revision": "f4e29dfb117b4a7029bc9b1f66a0b24c",
+    "url": "./static/media/screen1_img9.f4e29dfb.png"
+  }
+]);
